630b0fed4bd30c48b6254c61a09212a95db38ee3
phuonguit2015 <phuonguit2015@gmail.com.vn>
2016-01-10 11:58:02 +0700

    Initial
    Khởi tạo project

---
+++ sites/all/libraries/ckeditor/config.js
@@ -0,0 +1,15 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	//.contentsCss = '../ckeditor/fonts/fonts.css';
+	//config.font_names = 'Alegreya Sans/Alegreya_Sans;' + config.font_names;
+	//config.font_names = 'PatrickHand Regular/PatrickHand-Regular;' + config.font_names;
+	//config.font_names = 'OpenSans CondLight/OpenSans-CondLight;' + config.font_names;
+
+};
